ed8907568ec162da0ade0102d84b6a1b94b1244b
Gabriel Brito <dev3@wegen.com.br>
2023-10-09 17:47:19 -0300

    feat: add GetExpenseFromIdRepositoryImplementation

--- lib/app/features/expense/data/repositories/get_expense_from_id_repository_implementation.dart
+++ lib/app/features/expense/data/repositories/get_expense_from_id_repository_implementation.dart
@@ -1,11 +1,13 @@
 import '../../domain/entities/expense_entity.dart';
 import '../../domain/repositories/get_expense_from_id_repository.dart';
+import '../datasources/get_expense_from_id_datasource.dart';
 
 class GetExpenseFromIdRepositoryImplementation
     implements GetExpenseFromIdRepository {
+  final GetExpenseFromIdDatasource _getExpenseFromIdDatasource;
+
+  GetExpenseFromIdRepositoryImplementation(this._getExpenseFromIdDatasource);
+
   @override
-  Future<ExpenseEntity> call(int id) {
-    // TODO: implement call
-    throw UnimplementedError();
-  }
+  Future<ExpenseEntity> call(int id) async => await _getExpenseFromIdDatasource(id);
 }
