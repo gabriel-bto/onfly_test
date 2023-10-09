c3483103da20b890c918c3a69955e9d08323b301
Gabriel Brito <gabrielfelipe3213@gmail.com>
2023-10-09 00:58:25 -0300

    refactor: dependency inversion with datasource

--- lib/app/features/expense/data/repositories/create_expense_repository_implementation.dart
+++ lib/app/features/expense/data/repositories/create_expense_repository_implementation.dart
@@ -1,10 +1,13 @@
 import '../../domain/entities/expense_entity.dart';
 import '../../domain/repositories/create_expense_repository.dart';
+import '../datasources/create_expense_datasource.dart';
 
 class CreateExpenseRepositoryImplementation implements CreateExpenseRepository {
+  final CreateExpenseDatasource _createExpenseDatasource;
+
+  CreateExpenseRepositoryImplementation(this._createExpenseDatasource);
+
   @override
-  Future<bool> call(ExpenseEntity expenseEntity) {
-    // TODO: implement call
-    throw UnimplementedError();
-  }
+  Future<bool> call(ExpenseEntity expenseEntity) async =>
+      await _createExpenseDatasource(expenseEntity);
 }
