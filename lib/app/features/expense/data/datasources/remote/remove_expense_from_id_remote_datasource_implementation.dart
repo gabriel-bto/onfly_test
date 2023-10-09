9c670cee7cc74e472458a648a6a03c0a8d436683
Gabriel Brito <dev3@wegen.com.br>
2023-10-09 17:47:56 -0300

    feat: add remote RemoveExpenseFromId

---
+++ lib/app/features/expense/data/datasources/remote/remove_expense_from_id_remote_datasource_implementation.dart
@@ -0,0 +1,17 @@
+import '../../../../../core/http_client/http_client_implementation.dart';
+import '../../../../../core/utils/api_utils.dart';
+import '../remove_expense_from_id_datasource.dart';
+
+class RemoveExpenseRemoteDatasourceImplementation
+    implements RemoveExpenseFromIdDatasource {
+  final HttpClientImplementation _httpClientImplementation;
+
+  RemoveExpenseRemoteDatasourceImplementation(this._httpClientImplementation);
+
+  @override
+  Future<bool> call(int id) async {
+    await _httpClientImplementation.get(ApiUtils.getRouteRemoveExpense(id));
+
+    return true;
+  }
+}
