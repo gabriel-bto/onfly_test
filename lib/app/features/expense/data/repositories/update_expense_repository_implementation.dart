2df90f985a54cb3351e98c431693a0073a093b69
Gabriel Brito <gabrielfelipe3213@gmail.com>
2023-10-09 01:00:43 -0300

    feat: add update expanse repository in data

---
+++ lib/app/features/expense/data/repositories/update_expense_repository_implementation.dart
@@ -0,0 +1,10 @@
+import '../../domain/entities/expense_entity.dart';
+import '../../domain/repositories/update_expense_repository.dart';
+
+class UpdateExpenseRepositoryImplementation implements UpdateExpenseRepository {
+  @override
+  Future<bool> call(ExpenseEntity expenseEntity) {
+    // TODO: implement call
+    throw UnimplementedError();
+  }
+}
