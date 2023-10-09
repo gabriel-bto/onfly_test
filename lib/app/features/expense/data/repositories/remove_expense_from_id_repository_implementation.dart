73dbd246de0ae82cb08a73fda513c2aadb5e1bb5
Gabriel Brito <dev3@wegen.com.br>
2023-10-09 17:34:46 -0300

    feat: add UpdateExpenseRepositoryImplementation

--- lib/app/features/expense/data/repositories/remove_expense_from_id_repository_implementation.dart
+++ lib/app/features/expense/data/repositories/remove_expense_from_id_repository_implementation.dart
@@ -1,10 +1,12 @@
 import '../../domain/repositories/remove_expense_from_id_repository.dart';
+import '../datasources/remove_expense_from_id_datasource.dart';
 
 class RemoveExpenseFromIdRepositoryImplementation
     implements RemoveExpenseFromIdRepository {
+  final RemoveExpenseFromIdDatasource _removeExpenseDatasource;
+
+  RemoveExpenseFromIdRepositoryImplementation(this._removeExpenseDatasource);
+
   @override
-  Future<bool> call(int id) {
-    // TODO: implement call
-    throw UnimplementedError();
-  }
+  Future<bool> call(int id) async => await _removeExpenseDatasource(id);
 }
