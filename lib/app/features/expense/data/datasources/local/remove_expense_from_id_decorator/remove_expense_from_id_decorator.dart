fbbfd4cbef620ac55ddb81b317983ab3f18065d9
Gabriel Brito <dev3@wegen.com.br>
2023-10-09 17:50:03 -0300

    feat: add RemoveExpenseFromId decorator

---
+++ lib/app/features/expense/data/datasources/local/remove_expense_from_id_decorator/remove_expense_from_id_decorator.dart
@@ -0,0 +1,10 @@
+import '../../remove_expense_from_id_datasource.dart';
+
+class RemoveExpenseFromIdDecorator implements RemoveExpenseFromIdDatasource {
+  final RemoveExpenseFromIdDatasource _removeExpenseFromIdDatasource;
+
+  RemoveExpenseFromIdDecorator(this._removeExpenseFromIdDatasource);
+
+  @override
+  Future<bool> call(int id) async => await _removeExpenseFromIdDatasource(id);
+}
---
+++ lib/app/features/expense/data/datasources/local/remove_expense_from_id_decorator/remove_expense_from_id_decorator_implementation.dart
@@ -0,0 +1,30 @@
+import '../../../../../../core/database/sqlite_connection_factory.dart';
+import 'remove_expense_from_id_decorator.dart';
+
+class RemoveExpenseFromIdDecoratorImplementation
+    extends RemoveExpenseFromIdDecorator {
+  RemoveExpenseFromIdDecoratorImplementation(
+      super.removeExpenseFromIdDatasource);
+
+  final _sqliteConnectionFactory = SqliteConnectionFactory();
+
+  @override
+  Future<bool> call(int id) async {
+    try {
+      await super(id);
+      await _removeInLocal(id);
+      return true;
+    } catch (_) {
+      await _removeInLocal(id);
+      return true;
+    }
+  }
+
+  Future<bool> _removeInLocal(int id) async {
+    final connection = await _sqliteConnectionFactory.openConnection();
+
+    await connection.rawDelete('delete from expense where id = ?', [id]);
+
+    return true;
+  }
+}
