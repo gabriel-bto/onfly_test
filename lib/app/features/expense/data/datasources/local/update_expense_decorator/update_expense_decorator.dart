add4b99ce12872f5228abac47f9c8005d806a20c
Gabriel Brito <dev3@wegen.com.br>
2023-10-09 17:35:51 -0300

    feat: add UpdateExpenseDecorator

---
+++ lib/app/features/expense/data/datasources/local/update_expense_decorator/update_expense_decorator.dart
@@ -0,0 +1,12 @@
+import '../../../../domain/entities/expense_entity.dart';
+import '../../update_expense_datasource.dart';
+
+class UpdateExpenseDecorator implements UpdateExpenseDatasource {
+  final UpdateExpenseDatasource _updateExpenseDatasource;
+
+  UpdateExpenseDecorator(this._updateExpenseDatasource);
+
+  @override
+  Future<bool> call(ExpenseEntity expenseEntity) async =>
+      await _updateExpenseDatasource(expenseEntity);
+}
