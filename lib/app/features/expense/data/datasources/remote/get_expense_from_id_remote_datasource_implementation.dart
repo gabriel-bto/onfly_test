83d9a0306c6e5d02d34adbadb578b3d9c008b125
Gabriel Brito <dev3@wegen.com.br>
2023-10-09 17:48:18 -0300

    feat: add remote GetExpenseFromId

---
+++ lib/app/features/expense/data/datasources/remote/get_expense_from_id_remote_datasource_implementation.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import '../../../../../core/http_client/http_client_implementation.dart';
+import '../../../../../core/utils/api_utils.dart';
+import '../../../domain/entities/expense_entity.dart';
+import '../../models/expense_model.dart';
+import '../get_expense_from_id_datasource.dart';
+
+class GetExpenseFromIdRemoteDatasourceImplementation
+    implements GetExpenseFromIdDatasource {
+  final HttpClientImplementation _httpClientImplementation;
+
+  GetExpenseFromIdRemoteDatasourceImplementation(
+    this._httpClientImplementation,
+  );
+
+  @override
+  Future<ExpenseEntity> call(int id) async {
+    var response = await _httpClientImplementation.get(
+      ApiUtils.getRouteGetExpense(id),
+    );
+
+    var json = jsonDecode(response.body);
+
+    return ExpenseModel.fromJson(json);
+  }
+}
