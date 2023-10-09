6ff4627d906879ecf6499da94a98ce99bb279044
Gabriel Brito <dev3@wegen.com.br>
2023-10-09 17:34:22 -0300

    feat: add UpdateExpenseRepository in data layer

--- lib/app/features/expense/data/repositories/update_expense_repository_implementation.dart
+++ lib/app/features/expense/data/repositories/update_expense_repository_implementation.dart
@@ -1,10 +1,13 @@
 import '../../domain/entities/expense_entity.dart';
 import '../../domain/repositories/update_expense_repository.dart';
+import '../datasources/update_expense_datasource.dart';
 
 class UpdateExpenseRepositoryImplementation implements UpdateExpenseRepository {
+  final UpdateExpenseDatasource _expenseDatasource;
+
+  UpdateExpenseRepositoryImplementation(this._expenseDatasource);
+
   @override
-  Future<bool> call(ExpenseEntity expenseEntity) {
-    // TODO: implement call
-    throw UnimplementedError();
-  }
+  Future<bool> call(ExpenseEntity expenseEntity) async =>
+      await _expenseDatasource(expenseEntity);
 }
