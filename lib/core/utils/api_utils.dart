045ebbfd966d170030d28d672a19033586bab07e
Gabriel Brito <gabrielfelipe3213@gmail.com>
2023-10-11 08:34:18 -0300

    fix: strings routes concatenation

--- lib/core/utils/api_utils.dart
+++ lib/core/utils/api_utils.dart
@@ -1,7 +1,8 @@
 abstract class ApiUtils {
   static const String baseURL = 'https://go-bd-api-3iyuzyysfa-uc.a.run.app/api';
 
-  static const String token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2xsZWN0aW9uSWQiOiJfcGJfdXNlcnNfYXV0aF8iLCJleHAiOjE2OTgwOTU3MzYsImlkIjoiZWhnMDJhaGxmaGR0emdtIiwidHlwZSI6ImF1dGhSZWNvcmQifQ.IK2RT5bjIzyRh1dlOeT0VZpGZTdNyBN1R30iOYa00sc';
+  static const String token =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2xsZWN0aW9uSWQiOiJfcGJfdXNlcnNfYXV0aF8iLCJleHAiOjE2OTgxOTAzOTEsImlkIjoiZWhnMDJhaGxmaGR0emdtIiwidHlwZSI6ImF1dGhSZWNvcmQifQ.fnV7xfE2iX8MKVLqe79e7isR8TAVwAO86z12d3eo1XU';
 
   static const String login = 'nrRUdBbW';
 
@@ -11,16 +12,18 @@ abstract class ApiUtils {
         'Authorization': token,
       };
 
-  static const String routeListExpenses = '$baseURL/collections/expense_$login/records';
+  static const String routeListExpenses =
+      '$baseURL/collections/expense_$login/records';
 
-  static String getRouteGetExpense(String id) => '$baseURL/collections/expense_$login/records/?id=$id';
+  static String getRouteGetExpense(String id) =>
+      '$baseURL/collections/expense_$login/records/$id';
 
   static const String routeCreateExpense =
       '$baseURL/collections/expense_$login/records';
 
   static String getRouteUpdateExpense(String id) =>
-      '$baseURL/collections/expense_$login/records/?id=$id';
+      '$baseURL/collections/expense_$login/records/$id';
 
   static String getRouteRemoveExpense(String id) =>
-      '$baseURL/collections/expense_$login/records/?id=$id';
+      '$baseURL/collections/expense_$login/records/$id';
 }
